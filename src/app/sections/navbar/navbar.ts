34b5be8f730578a282cd226a130d7ee1831d79e0
Kevin Arnold Flores Pacheco <arnold_k2001@hotmail.com>
2025-10-17 14:28:24 -0500

    feat: integrate Lucide icons into navbar for social links

--- src/app/sections/navbar/navbar.ts
+++ src/app/sections/navbar/navbar.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { LucideAngularModule, FacebookIcon, InstagramIcon, LinkedinIcon } from 'lucide-angular';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
-
+  readonly FacebookIcon = FacebookIcon;
+  readonly InstagramIcon = InstagramIcon;
+  readonly LinkedinIcon = LinkedinIcon;
 }
