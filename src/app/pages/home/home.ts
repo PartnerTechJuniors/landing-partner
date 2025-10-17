7b8b27329f501e0d995fb5e61e285daa024426c4
Kevin Arnold Flores Pacheco <arnold_k2001@hotmail.com>
2025-10-17 15:07:02 -0500

    feat: add About section with template, styles, and tests

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { About } from '@sections/about/about';
 import { Hero } from '@sections/hero/hero';
 import { Navbar } from '@sections/navbar/navbar';
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar, Hero],
+  imports: [Navbar, Hero, About],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
